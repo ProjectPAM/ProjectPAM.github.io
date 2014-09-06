77f46a8ef11d8ede9f121c91d816f939cb0aa0ab
Daniel Olsen <dolsen@siu.edu>
2014-09-05 19:18:29 -0500

    Remove </script>

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,7 +9,7 @@
 	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
 	
 	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+}(document, 'script', 'facebook-jssdk'));
 	
 !function(d,s,id)
 {
